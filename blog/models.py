7f4094342ca6c1300d326b2208f5eec631814892
AibarIbray <aibar.s.ibrayev@gmail.com>
2021-11-17 15:36:55 +0600

    new start

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+    #add links to read more
+    def __str__(self):
+        return self.title
+    #def summary(self):
+    #    return self.body[:100]
--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    #body = models.TextField()
+    #add links to facebook and email
+
+    def __str__(self):
+        return self.summary

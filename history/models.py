c7753b0c06a1e8aa0b3a54bfa7c9bbd0baa0a2ae
AibarIbray <aibar.s.ibrayev@gmail.com>
2021-11-18 13:03:43 +0600

    finished the History

--- history/models.py
+++ history/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class History(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    readmore = models.URLField(max_length=1000, default='')
+    def __str__(self):
+        return self.title
--- history/views.py
+++ history/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import History
 
-# Create your views here.
+def allhistory(request):
+    history = History.objects
+    return render(request, 'history/allhistory.html', {'history':history})
